2e3db7eedd109594d97a1548e5c0c824248278db
guilherme <caliariguilherme8@gmail.com>
2026-06-15 19:38:06 -0300

    initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+import pygame
+
+print('Game Start')
+pygame.init()
+
+window = pygame.display.set_mode(size=(640, 480))
+print('Game End')
+
+print('Loop Start')
+while True:
+    #Check for all events
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit() # Close window
+            quit() # End pygame
+
+
